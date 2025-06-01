08cfb533a8ad9ac5307c3798fd8dad123843589c
AnanthuNarashimman <ananthu.narashimman@gmail.com>
2025-06-01 16:45:29 +0530

    Port changes for render

--- backend/app.py
+++ backend/app.py
@@ -471,5 +471,5 @@ def deleteReminder():
         return jsonify({"message": "Internal server error"}), 500
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 10000))
     app.run(host='0.0.0.0', port=port, debug=False)
