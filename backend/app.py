73d11ac2cf1de6d5d888c22e5db22dcf4356cb5f
AnanthuNarashimman <ananthu.narashimman@gmail.com>
2025-06-01 16:56:41 +0530

    removed monkeypatch

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,5 @@
-import eventlet
-eventlet.monkey_patch()
+# import eventlet
+# eventlet.monkey_patch()
 
 from flask import Flask, request, jsonify, session, redirect
 from datetime import datetime, timedelta
